798cb1f332e7cc197ff1c9d74ae56e746781244b
Jon Hilton <hilton.jon@gmail.com>
2019-10-18 17:48:39 +0100

    Add project files.

---
+++ Controllers/TweetController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyTweets.Features.Tweets;
+
+namespace MyTweets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TweetController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public TweetController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var model = await mediator.Send(new List.Query());
+            return Ok(model);
+        }
+    }
+}
---
+++ Features/Tweets/List.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyTweets.Features.Tweets
+{
+    public class List
+    {
+        public class Query : IRequest<Model>
+        {
+        }
+
+        public class Model
+        {
+            public List<string> Tweets { get; set; }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, Model>
+        {
+            public async Task<Model> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return new Model
+                {
+                    Tweets = new List<string>
+                    {
+                        "One from the server",
+                        "Two from the server",
+                        "Three from the server",
+                        "Four"
+                    }
+                };
+            }
+        }
+    }
+}
